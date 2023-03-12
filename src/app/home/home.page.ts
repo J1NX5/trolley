49077287d54c953237497558d0c9dbff19a90b25
J1NX5 <j1inx@posteo.de>
2023-03-12 22:42:07 +0100

    init commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items: number[] = [];
+  article: string = "";
+  shopping_cart: number = 0;
+
+  constructor(private toastController: ToastController) {}
+  addNum(num_string: string){
+    this.article += num_string
+  }
+
+  addNumToList(){
+    const newItemValue = parseFloat(this.article);
+    if (!isNaN(newItemValue)) {
+      this.items.push(parseFloat(this.article))  
+      this.delNum()
+      this.calCart()
+    } else {
+      this.toastController.create({
+        message: 'Bang. Mach das nicht noch einmal!',
+        duration: 2000,
+      }).then(toast => toast.present());
+    }
+
+  }
+
+  delNum(){
+    this.article = ""
+  }
+
+  delItem(del_num_index: number){
+    this.items.splice(del_num_index, 1)
+    this.calCart()
+  }
+
+  calCart(){
+    let tmp_sc: number = 0
+    for(var i = 0; i < this.items.length; i++){
+      tmp_sc += this.items[i]
+    }
+    this.shopping_cart = tmp_sc
+  }
 
 }
